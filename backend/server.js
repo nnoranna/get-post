337873962397437298691e35552c721b643845ed
nnoranna <anagy.nora.anna@gmail.com>
2022-03-25 15:52:07 +0100

    Install package, create gitignore

--- backend/server.js
+++ backend/server.js
@@ -1,69 +1,57 @@
-const http = require('http');
+/* const http = require('http');
 const fs = require('fs');
 const path = require('path');
-
-const server = http.createServer((req, res) => {
-
-	const errorHTML = `
-		
-	<!DOCTYPE html>
-	<html lang="en">
-	<head>
-		<meta charset="UTF-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<link rel="preconnect" href="https://fonts.googleapis.com"> 
-		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> 
-		<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@800&display=swap" rel="stylesheet">
-		<style>
-			body{
-				padding: 0; margin: 0;
-				font-family: 'Montserrat', sans-serif;
-				font-weight: 800;
-				background-color: #4343F9;
-				color: #fff;
-			}
-			#root{
-				width: 100%;
-				height: 100vh;
-				display: flex;
-				justify-content: center;
-				align-items: center;
-				font-size: 21px;
-			}
-		</style>
-		<title>Not here</title>
-	</head>
-	<body>
-		<div id="root">Rise your gaze to the sky<br/>than a bit back to the URL bar<br/>and check that link again</div>
-	</body>
-	</html>
-	
-	`;
-    
-	let filePath = path.resolve(__dirname + '/../frontend' + req.url);
-    
-	fs.access(filePath, fs.constants.R_OK, (err) => {
-	if(err){
-		res.statusCode = 404;
-		res.end(errorHTML);
-	}else{
-		if(fs.statSync(filePath).isDirectory()) {
-			filePath += '/index.html';
-		}
-		fs.readFile(filePath, (err, data) => {
-			if(err) {
-				res.statusCode = 500;
-				res.end(errorHTML);
-			} else {
-				res.end(data);
-			}
-		});
-	}
-	});
-});
-
-server.listen(9000, "127.0.0.1", () => {
+const port = 9000;
+const ipAdress = "127.0.0.1";
+const listenFun = () => {
     const addr = server.address();
-		console.log(`http://${addr.address}:${addr.port}`);
+		console.log(`http://${addr.address}:${addr.port}`); //nem a browserbe írja ki, hanem terminalba
+		console.log(`Everything is going to be alright`);
+	}
+
+const serverFun = (req, res) => {
+
+		const errorHTML = `You lost your way`;
+		
+		//let filePath = path.resolve(__dirname + '/../frontend' + req.url);
+		let filePath = path.resolve(`${__dirname}/../frontend${req.url}`);
+		
+		fs.access(filePath, fs.constants.R_OK, (err) => {
+		if(err){
+			res.statusCode = 404;
+			res.end(errorHTML);
+		}else{
+			if(fs.statSync(filePath).isDirectory()) {
+				filePath += '/index.html';
+			}
+			fs.readFile(filePath, (err, data) => {
+				if(err) {
+					res.statusCode = 500;
+					res.end(errorHTML);
+				} else {
+					console.log("az index html kiszolgálódott");
+					res.end(data);
+				}
+			});
+		}
+		});
+}
+
+const server = http.createServer(serverFun);
+
+server.listen(port, ipAdress, listenFun); */
+
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 9000;
+
+app.get("/", (req, res) => {
+	res.sendFile(path.join(`${__dirname}/../frontend/index.html`))
 });
+
+app.use("/pub", express.static(`${__dirname}/../frontend/pub`));
+
+app.listen(port, () => {
+	console.log("http://127.0.0.1:9000");
+})
